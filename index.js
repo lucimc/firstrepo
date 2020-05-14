64f007c9c1d6838ceb85d9c48e651e94dd8efbb1
lucimc <lucimc@gmail.com>
2020-05-14 19:28:55 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //ez egy megjegyzés
 // ez a második megjegyzés
 // ez a 3. megjegyzés a konflikthoz
-
+//valami de igen
 const calculator = require (./calculator.js);
 console.log('Hello World!');
 
